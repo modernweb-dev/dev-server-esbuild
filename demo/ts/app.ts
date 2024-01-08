dca81fc9a0b1ff4a8398c88b54b66a109b6c6e07
Kristján Oddsson <koddsson@gmail.com>
2024-01-08 15:30:11 +0100

    set up linting

--- demo/ts/app.ts
+++ demo/ts/app.ts
@@ -1,6 +1,7 @@
 import { LitElement, customElement, property, html } from 'lit-element';
 
 @customElement('my-app')
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class MyApp extends LitElement {
   @property({ type: String })
   foo = 'bar';
